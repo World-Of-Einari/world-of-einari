c05b370d60d5f3720714d2c0d543e24be33deb28
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2026-03-26 15:57:23 +0000

    feat: disable chat

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './layout/nav/nav.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { CursorComponent } from './shared/components/cursor/cursor.component';
-import { ChatComponent } from './features/chat/chat.component';
+// import { ChatComponent } from './features/chat/chat.component';
 
 @Component({
   selector: 'en-root',
   templateUrl: './app.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, NavComponent, FooterComponent, CursorComponent, ChatComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, CursorComponent /* ChatComponent */],
 })
 export class AppComponent {}
